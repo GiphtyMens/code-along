266084e394eb63af311d91a2f9c01a65fdfef52c
Gifty Mensah <giftymens111@gmail.com>
2022-04-04 13:59:19 +0000

    react-component-8

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+        <h1>Welcome to the About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        reiciendis pariatur recusandae enim consequatur praesentium repellendus
+        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
+        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
+        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
+        itaque voluptatum optio quidem aliquam iusto voluptatem illum
+        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
+        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
+        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
+        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
+        nostrum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        reiciendis pariatur recusandae enim consequatur praesentium repellendus
+        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
+        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
+        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
+        itaque voluptatum optio quidem aliquam iusto voluptatem illum
+        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
+        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
+        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
+        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
+        nostrum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+        reiciendis pariatur recusandae enim consequatur praesentium repellendus
+        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
+        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
+        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
+        itaque voluptatum optio quidem aliquam iusto voluptatem illum
+        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
+        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
+        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
+        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
+        nostrum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
