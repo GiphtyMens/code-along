9a47df583c8efb953fa8c8887caa4eb907fee5e2
Gifty Mensah <giftymens111@gmail.com>
2022-04-05 14:14:22 +0000

    react-component-9

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,50 +1,8 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 
-const Blog = () => {
-  return (
-    <section>
-        <h1>Welcome to the Blog Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        reiciendis pariatur recusandae enim consequatur praesentium repellendus
-        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
-        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
-        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
-        itaque voluptatum optio quidem aliquam iusto voluptatem illum
-        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
-        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
-        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
-        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
-        nostrum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        reiciendis pariatur recusandae enim consequatur praesentium repellendus
-        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
-        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
-        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
-        itaque voluptatum optio quidem aliquam iusto voluptatem illum
-        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
-        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
-        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
-        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
-        nostrum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-        reiciendis pariatur recusandae enim consequatur praesentium repellendus
-        illum, nostrum a laudantium nemo sapiente nisi harum ratione facere
-        tenetur dolorem? Vitae, fugit! Consequuntur in vel iure suscipit
-        mollitia dignissimos aliquam voluptatibus nulla. Animi quas, distinctio
-        itaque voluptatum optio quidem aliquam iusto voluptatem illum
-        consectetur corrupti dolorum nihil. Nihil illo error possimus impedit?
-        Amet eum fuga corporis necessitatibus pariatur? Harum suscipit commodi
-        reprehenderit nihil deleniti recusandae eos ad veritatis atque provident
-        reiciendis quas labore porro fugiat animi cum, illum quisquam a dolores
-        nostrum.
-      </p>
-    </section>
-  );
-};
+
+function Blog() {
+  return <Outlet />;
+}
 
 export default Blog;
